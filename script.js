159a66e18740c56bf76d234de261fea50b13f866
CSG <granadossanchezcristina@gmail.com>
2025-12-19 20:05:49 +0100

    cambios

---
+++ script.js
@@ -0,0 +1,25 @@
+const  content = document.getElementById("content");
+const links = document.querySelectorAll(".load-page");
+
+async function loadPage(page) {
+    try {
+        const response = await fetch(page);
+        const html = await response.text();
+        content.innerHTML = html;
+        content.scrollTop = 0;
+    } catch (error) {
+        content.innerHTML = "<p>Error al cargar el contenido</p>";
+    }
+}
+
+// Click en enlaces
+links.forEach(link => {
+        link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const page = link.dataset.page;
+        loadPage(page);
+    });
+});
+
+// Cargar página inicial
+loadPage("contenido/todo.html");
